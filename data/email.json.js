b1269f5fc6dae4a9f0cf3366f169fb654f3f347e
wangyanan <wangyanan@education-dynamics.com>
2021-01-26 15:40:53 +0800

    first commit

---
+++ data/email.json.js
@@ -0,0 +1,165 @@
+export default {
+  "contacts": [
+    {
+      "id": 1,
+      "name": "Grace Johnson",
+      "email": "grace@gmail.com",
+      "selected": false
+    },
+    {
+      "id": 2,
+      "name": "John Rodriguez",
+      "email": "john@shineteachchina.com",
+      "selected": false
+    },
+    {
+      "id": 3,
+      "name": "Frank Brown",
+      "email": "frank@qq.com",
+      "selected": false
+    },
+    {
+      "id": 4,
+      "name": "Tina Johnson",
+      "email": "tina@foxmail.com",
+      "selected": false
+    },
+    {
+      "id": 5,
+      "name": "Mike Brown",
+      "email": "mike@gmail.com",
+      "selected": false
+    },
+    {
+      "id": 6,
+      "name": "Sunshine Wilson",
+      "email": "sunshine@shineteachchina.com",
+      "selected": false
+    },
+    {
+      "id": 7,
+      "name": "Jerry Johnson",
+      "email": "jerry@gmail.com",
+      "selected": false
+    },
+    {
+      "id": 8,
+      "name": "Coco Wilson",
+      "email": "coco@shineteachchina.com",
+      "selected": false
+    },
+    {
+      "id": 9,
+      "name": "Alice Rodriguez",
+      "email": "alice@gmail.com",
+      "selected": false
+    },
+    {
+      "id": 10,
+      "name": "Elsa Brown",
+      "email": "elsa@foxmail.com",
+      "selected": false
+    },
+    {
+      "id": 11,
+      "name": "Emily Johnson",
+      "email": "emily@gmail.com",
+      "selected": false
+    },
+    {
+      "id": 12,
+      "name": "Apple Rodriguez",
+      "email": "apple@gmail.com",
+      "selected": false
+    },
+    {
+      "id": 13,
+      "name": "Orange Johnson",
+      "email": "orange@shineteachchina.com",
+      "selected": false
+    },
+    {
+      "id": 14,
+      "name": "Banana Brown",
+      "email": "banana@qq.com",
+      "selected": false
+    }
+  ],
+  "groups": [
+    {
+      "groupName": "colleague",
+      "groupId": 1,
+      "selected": false,
+      "members": [
+        {
+          "id": 2,
+          "name": "John Rodriguez",
+          "email": "john@shineteachchina.com",
+          "selected": false
+        },
+        {
+          "id": 3,
+          "name": "Frank Brown",
+          "email": "frank@qq.com",
+          "selected": false
+        },
+        {
+          "id": 4,
+          "name": "Tina Johnson",
+          "email": "tina@foxmail.com",
+          "selected": false
+        }
+      ]
+    },
+    {
+      "groupName": "friend",
+      "groupId": 2,
+      "selected": false,
+      "members": [
+        {
+          "id": 4,
+          "name": "Tina Johnson",
+          "email": "tina@foxmail.com",
+          "selected": false
+        },
+        {
+          "id": 5,
+          "name": "Mike Brown",
+          "email": "mike@gmail.com",
+          "selected": false
+        }
+      ]
+    },
+    {
+      "groupName": "family",
+      "groupId": 3,
+      "selected": false,
+      "members": [
+        {
+          "id": 8,
+          "name": "Coco Wilson",
+          "email": "coco@shineteachchina.com",
+          "selected": false
+        },
+        {
+          "id": 9,
+          "name": "Alice Rodriguez",
+          "email": "alice@gmail.com",
+          "selected": false
+        },
+        {
+          "id": 10,
+          "name": "Elsa Brown",
+          "email": "elsa@foxmail.com",
+          "selected": false
+        },
+        {
+          "id": 11,
+          "name": "Emily Johnson",
+          "email": "emily@gmail.com",
+          "selected": false
+        }
+      ]
+    }
+  ]
+}
---
+++ data/phone.json.js
@@ -0,0 +1,165 @@
+export default {
+  "contacts": [
+    {
+      "id": 1,
+      "name": "Grace Johnson",
+      "phone": "15837102903",
+      "selected": false
+    },
+    {
+      "id": 2,
+      "name": "John Rodriguez",
+      "phone": "15232102903",
+      "selected": false
+    },
+    {
+      "id": 3,
+      "name": "Frank Brown",
+      "phone": "15823572903",
+      "selected": false
+    },
+    {
+      "id": 4,
+      "name": "Tina Johnson",
+      "phone": "15823575678",
+      "selected": false
+    },
+    {
+      "id": 5,
+      "name": "Mike Brown",
+      "phone": "15878945678",
+      "selected": false
+    },
+    {
+      "id": 6,
+      "name": "Sunshine Wilson",
+      "phone": "15878943413",
+      "selected": false
+    },
+    {
+      "id": 7,
+      "name": "Jerry Johnson",
+      "phone": "13838043413",
+      "selected": false
+    },
+    {
+      "id": 8,
+      "name": "Coco Wilson",
+      "phone": "15938043413",
+      "selected": false
+    },
+    {
+      "id": 9,
+      "name": "Alice Rodriguez",
+      "phone": "18938043413",
+      "selected": false
+    },
+    {
+      "id": 10,
+      "name": "Elsa Brown",
+      "phone": "15838043413",
+      "selected": false
+    },
+    {
+      "id": 11,
+      "name": "Emily Johnson",
+      "phone": "13838043422",
+      "selected": false
+    },
+    {
+      "id": 12,
+      "name": "Apple Rodriguez",
+      "phone": "13888043413",
+      "selected": false
+    },
+    {
+      "id": 13,
+      "name": "Orange Johnson",
+      "phone": "13888043333",
+      "selected": false
+    },
+    {
+      "id": 14,
+      "name": "Banana Brown",
+      "phone": "13888043245",
+      "selected": false
+    }
+  ],
+  "groups": [
+    {
+      "groupName": "colleague",
+      "groupId": 1,
+      "selected": false,
+      "members": [
+        {
+          "id": 2,
+          "name": "John Rodriguez",
+          "phone": "15232102903",
+          "selected": false
+        },
+        {
+          "id": 3,
+          "name": "Frank Brown",
+          "phone": "15823572903",
+          "selected": false
+        },
+        {
+          "id": 4,
+          "name": "Tina Johnson",
+          "phone": "15823575678",
+          "selected": false
+        }
+      ]
+    },
+    {
+      "groupName": "friend",
+      "groupId": 2,
+      "selected": false,
+      "members": [
+        {
+          "id": 4,
+          "name": "Tina Johnson",
+          "phone": "15823575678",
+          "selected": false
+        },
+        {
+          "id": 5,
+          "name": "Mike Brown",
+          "phone": "15878945678",
+          "selected": false
+        }
+      ]
+    },
+    {
+      "groupName": "family",
+      "groupId": 3,
+      "selected": false,
+      "members": [
+        {
+          "id": 8,
+          "name": "Coco Wilson",
+          "phone": "15938043413",
+          "selected": false
+        },
+        {
+          "id": 9,
+          "name": "Alice Rodriguez",
+          "phone": "18938043413",
+          "selected": false
+        },
+        {
+          "id": 10,
+          "name": "Elsa Brown",
+          "phone": "15838043413",
+          "selected": false
+        },
+        {
+          "id": 11,
+          "name": "Emily Johnson",
+          "phone": "13838043422",
+          "selected": false
+        }
+      ]
+    }
+  ]
+}
